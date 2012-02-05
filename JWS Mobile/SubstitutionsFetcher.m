4b78be36a18f3129f8541169fda3e7ee67a029b8
Björn <baze@Bjoern-Martensens-iMac.local>
2012-02-05 15:55:05 +0100

    minor cleanup to substitutionFetcher

--- JWS Mobile/SubstitutionsFetcher.m
+++ JWS Mobile/SubstitutionsFetcher.m
@@ -15,13 +15,10 @@
 + (NSArray *)executeSubstitutionsFetch:(NSString *)query
 {
     //query = [NSString stringWithFormat:@"%@&format=json&nojsoncallback=1&api_key=%@", query, FlickrAPIKey];
-    //query = [query stringByAppendingPathComponent:@"tage"];
-    // NSLog(@"[%@ %@] sent %@", NSStringFromClass([self class]), NSStringFromSelector(_cmd), query);
     NSData *jsonData = [[NSString stringWithContentsOfURL:[NSURL URLWithString:query] encoding:NSUTF8StringEncoding error:nil] dataUsingEncoding:NSUTF8StringEncoding];
     NSError *error = nil;
     NSArray *results = jsonData ? [NSJSONSerialization JSONObjectWithData:jsonData options:NSJSONReadingMutableContainers|NSJSONReadingMutableLeaves error:&error] : nil;
     if (error) NSLog(@"[%@ %@] JSON error: %@", NSStringFromClass([self class]), NSStringFromSelector(_cmd), error.localizedDescription);
-    // NSLog(@"[%@ %@] received %@", NSStringFromClass([self class]), NSStringFromSelector(_cmd), results);
     return results;
 }
 
@@ -31,8 +28,5 @@
     NSString *request = [NSString stringWithFormat:@"%@", BASE_URL];
     return [self executeSubstitutionsFetch:request];
 }
-
-
-
 
 @end
