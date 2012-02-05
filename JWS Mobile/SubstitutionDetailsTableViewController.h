237dc2209eccb852201ef1643f59e544ea0fa859
Björn <baze@Bjoern-Martensens-iMac.local>
2012-02-05 12:37:19 +0100

    add Details ViewController

---
+++ JWS Mobile/SubstitutionDetailsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SubstitutionDetailsTableViewController.h
+//  JWS Mobile
+//
+//  Created by Björn Martensen on 05.02.12.
+//  Copyright (c) 2012 eberle & wollweber COMMUNICATIONS GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SubstitutionDetailsTableViewController : UITableViewController
+
+@end
